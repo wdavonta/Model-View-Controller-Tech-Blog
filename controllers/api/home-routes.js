f0b07a76c48914c2926c0ce044addf69f76a4e5a
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-02-07 09:41:13 -0500

    updated models

---
+++ controllers/api/home-routes.js
@@ -0,0 +1,1 @@
+const {format_date} = require ('../utils/helpers');
---
+++ models/Index.js
@@ -0,0 +1,38 @@
+// import models
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+
+// Post belongsTo User
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+// User have many Posts
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+// Comment belongTo Tags
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+// Post belongTo Products
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+//Post have many Comment
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+// User have many Comment
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+
+module.exports = {
+  Post,
+  Comment,
+  User
+};
