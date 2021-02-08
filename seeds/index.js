3b84826a2670923c7693d3ba4f4a53363d15fdfe
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-02-08 00:06:41 -0500

    update

--- seeds/index.js
+++ seeds/index.js
@@ -14,8 +14,8 @@ const seedAll = async () => {
   await seedPosts();
   console.log('--------------');
 
-  await seedComments();
-  console.log('--------------');
+  // await seedComments();
+  // console.log('--------------');
 
 
   process.exit(0);
